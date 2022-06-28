2facc02eb56503a8fac19bd84026c18ccdbc394c
Abuya Obunga <vabuyia@gmail.com>
2022-06-29 01:37:42 +0300

    Update the nav bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './index.css'
 
-console.log('Hello JS');
+const navSlide = () => {
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  const navLinks = document.querySelectorAll('.nav-links li')
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+  })
+  navLinks.forEach((link, index)=> {
+    link.style.animation ='navLinkFade o.5s ease forwards ${index /7 }s'
+
+
+  });
+}
+
+navSlide()
