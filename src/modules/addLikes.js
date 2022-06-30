55d2cc262c67dc37964568c87be245af4c056f71
Joffrey <joyjoffrey@gmail.com>
2022-06-30 07:48:16 +0200

    implement popup comment

--- src/modules/addLikes.js
+++ src/modules/addLikes.js
@@ -10,8 +10,32 @@ const addLikes = () => {
   })
 }
 
-const loadPopUpComment = (data) => {
-  
+const listComments = (comments) => {
+  let div = '';
+  comments.forEach((comment) => {
+    div += `<p class='popup-single-comment' > ${comment.creation_date} ${comment.username}:  ${comment.comment}</p>`;
+  });
+  return div;
+}
+
+const loadPopUpComment = ({ commentData, food}) => {
+  const ul = document.getElementById('pop');
+  ul.innerHTML = '';
+  const li = document.createElement('li');
+  li.innerHTML = `
+  <div class='comment-pop-image'> <img src='${food.strCategoryThumb}' alt='${food.strCategory}' /> <span> &#10006; </span>
+  <h4>${food.strCategory} </h4>
+  <p>${food.strCategoryDescription} </p>
+  <p>${commentData.length>1?'comments ('+commentData.length+')':'comment ('+commentData.length+')'} </p>
+  <div class='popup-comments'>${listComments(commentData)}</div>
+  <h5>add a comment</h5>
+  <form>
+  <input type='text' name='username' id='username' placeholder= 'Your Name' /> <br/>
+  <textarea placeholder='your insights' name='comment' id='comment'></textarea><br/>
+  <button type='submit' >Comment</button>
+  </form>
+  `;
+  ul.appendChild(li)
 }
 
 const popUpComment = () => {
